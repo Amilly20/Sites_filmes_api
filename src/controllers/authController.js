77de3c576699a254984b621aed3b110636b0c05e
Caio <caioosalencar@gmail.com>
2025-09-14 14:59:29 -0400

    fazendo toda parte de login

---
+++ src/controllers/authController.js
@@ -0,0 +1,35 @@
+import AuthService from '../services/authService.js';
+import { z } from 'zod';
+import { APIErro } from "../utils/ApiError.js";
+import { sendError, sendResponse } from "../utils/messages.js";
+
+class Autenticacao {
+    static login = async (req, res) => {
+        try {
+            const response = await authService.login(req.body)
+
+            return sendResponse(res, 200, {
+                data: response
+            });
+        } catch (error) {
+            if (error instanceof APIErro) {
+                const { code, errors } = error.toJson()
+                return sendError(res, code, ...errors)
+            }
+
+            if (error instanceof z.ZodError) {
+                let errors = []
+                error.issues.map((issue) => (
+                    errors.push({
+                        path: issue.path[0],
+                        message: issue.message
+                    })))
+                return sendError(res, 400, errors)
+            }
+
+            return sendError(res, 500, [])
+        }
+    }
+}
+
+export default Autenticacao;
